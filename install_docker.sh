fc251c8a3020168e81c76bdfd5aaeaa16105f36e
onsoim <onsoim@gmail.com>
2019-02-10 03:13:45 +0900

    start dockerize

---
+++ install_docker.sh
@@ -0,0 +1,11 @@
+# package update and upgrade
+sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get install -y docker-ce docker-compose
+sudo usermod -aG docker $USER
