0cc702e355525004246ab8560fca008dfed81959
onsoim <onsoim@gmail.com>
2019-02-10 03:38:14 +0900

    add -y option to apt-get

--- install_docker.sh
+++ install_docker.sh
@@ -1,11 +1,13 @@
 # package update and upgrade
-sudo apt-get upgrade
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sleep 3
 
 sudo apt-get install -y docker-ce docker-compose
 sudo usermod -aG docker $USER
