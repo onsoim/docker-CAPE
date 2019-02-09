595042c09dd31cf6e6372a1d1ac5356b9069a0b1
onsoim <onsoim@gmail.com>
2019-02-10 03:15:35 +0900

    tried to install CAPE on Ubuntu16.04

---
+++ install_CAPE.sh
@@ -0,0 +1,13 @@
+sudo apt-get install -y git vim \
+						python-pip \
+						python3-pip \
+						libgeoip-dev \
+						libvirt-dev \
+						libfuzzy-dev
+
+# python2: pefile2, geoip
+# python3: else
+pip install -r requirements-2.txt
+pip3 install -r requirements-3.txt
+
+python3 CAPE/cuckoo.py
