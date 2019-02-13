708fa246634f27647d2e6b96d793825d54b95030
onsoim <onsoim@gmail.com>
2019-02-14 04:09:26 +0900

    docker for CAPE

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,7 +1,12 @@
 # docker build -t onsoim/python2 .
-docker build -t onsoim/cuckoo .
+docker build -t onsoim/cape .
 
 echo "===== run docker test ====="
 
 docker rmi -f $(docker images -f "dangling=true" -q)
-docker run --rm onsoim/cuckoo cuckoo
+
+# docker run --rm onsoim/python2 python --version
+# docker run --rm -it onsoim/cuckoo /bin/bash
+
+docker system df
+docker volume ls
